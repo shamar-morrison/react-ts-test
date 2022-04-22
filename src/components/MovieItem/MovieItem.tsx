fb13af3620128b058b245c2f87c52c3667dfaf54
shamar-morrison <shamar.webdev@gmail.com>
2022-04-22 04:32:51 -0500

    Added Movie Images and Title

--- src/components/MovieItem/MovieItem.tsx
+++ src/components/MovieItem/MovieItem.tsx
@@ -1,10 +1,44 @@
-import styles from "/.MovieItem.module.scss";
+import styles from "./MovieItem.module.scss";
+import { MovieResult } from "src/types";
+import { BASE_IMG_URL } from "./../../paths";
+import defaultImage from "src/img/noImgFound.png";
+import { useState } from "react";
 
-const MovieItem = () => {
+interface Props {
+  movie: MovieResult;
+}
+
+const MovieItem = ({ movie }: Props) => {
+  const [movieTitle] = useState(
+    movie.title || movie.original_title || "Unknown Movie Title"
+  );
+
+  function buildPosterPath() {
+    return movie.poster_path
+      ? `${BASE_IMG_URL}${movie.poster_path}`
+      : defaultImage;
+  }
+
+  function makeSlug(title: string) {
+    return title
+      .toLowerCase()
+      .replace(/[^\w ]+/g, "")
+      .replace(/ +/g, "-");
+  }
+  const slug = makeSlug(movieTitle);
+
   return (
-    <div>
-      <p>Movie Item</p>
-    </div>
+    <a
+      href={`https://www.themoviedb.org/movie/${movie.id}-${slug}`}
+      target="_blank"
+    >
+      <div className={styles.container}>
+        <div className={styles.movieImage}>
+          <img src={buildPosterPath()} alt={movieTitle + " poster"} />
+        </div>
+        <p className={styles.title}>{movieTitle}</p>
+      </div>
+    </a>
   );
 };
 
--- src/components/MoviesWrapper/MoviesWrapper.tsx
+++ src/components/MoviesWrapper/MoviesWrapper.tsx
@@ -1,10 +1,37 @@
 import styles from "./MoviesWrapper.module.scss";
-import MovieItem from "./../MovieItem/MovieItem";
+import MovieItem from "src/components/MovieItem/MovieItem";
+import { useEffect, useState } from "react";
+import { MovieResult, Result } from "src/types";
 
 const MoviesWrapper = () => {
+  const [page, setPage] = useState(1);
+  const [movies, setMovies] = useState<MovieResult[]>([]);
+
+  async function getMovieList() {
+    try {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_API}&language=en-US&page=${page}`
+      );
+
+      if (data.status !== 200) throw Error("Error fetching movies");
+      const res: Result = await data.json();
+
+      setMovies((prev) => [...prev, ...res.results]);
+      console.log(res.results);
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    getMovieList();
+  }, [page]);
+
   return (
     <div className={styles.container}>
-      <MovieItem />
+      {movies.map((movie) => (
+        <MovieItem key={movie.id} movie={movie} />
+      ))}
     </div>
   );
 };
