5615da7185374acf6229d9fc99d2ae66db9f7c3d
shamar-morrison <shamar.webdev@gmail.com>
2022-04-24 01:02:00 -0500

    Added document title and custom favicon

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./App.module.scss";
 import MoviesWrapper from "./components/MoviesWrapper/MoviesWrapper";
 
 const App = () => {
+  document.title = "Top 500 TMDB Movies";
   return (
     <div className={styles.container}>
       <h2>Top 500 TMDB Movies List</h2>
